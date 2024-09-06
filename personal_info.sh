cb93bf0a64b4c7254892611fb206f2ff86d3e7af
pengchao2022 <pengchao.ma2@outlook.com>
2024-09-06 08:57:44 +0800

    Added personal input and output file

---
+++ personal_info.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Please enter your name here:"
+
+read name
+
+echo "Please enter your gender here:"
+
+read gender
+
+echo "Please enter your Age here:"
+
+read age
+
+echo "Your name is:$name"
+
+echo "Your gender is:$gender"
+
+echo "Your age is: $age"
+
+echo "You have completed the personal info input"
+
+echo "Thanks, Bye-bye!"
+
+
+
