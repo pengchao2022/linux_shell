9e9dbce847d9299839bba623c539929c4302addd
pengchao2022 <pengchao.ma2@outlook.com>
2024-09-06 15:01:09 +0800

    Added a simple for loop

---
+++ for01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# A simple for loop
+
+
+for (( i=1; i<=10; i++ ))
+
+do
+
+	echo $i
+
+done
+
+
---
+++ select.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# this script is to test the select syntax
+
+select name in allen ruby wendy mki
+
+do
+
+	echo "$name selected"
+
+done
+
+
