9bfef7199a0548b92e179449ad630c187bb0bb34
pengchao2022 <pengchao.ma2@outlook.com>
2024-09-06 12:13:40 +0800

    Added array study

---
+++ array.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+os=('Windows' 'Ubuntu' 'RHEL' 'CentOS' 'Unix')
+
+echo "${os[@]}"
+
+echo "${os[2]}"
+
+echo "${os[1]}"
+
+echo "${os[0]}"
+
+echo "${os[3]}"
+
+echo "${os[4]}"
+
+
