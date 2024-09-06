578a0685e1809974e6d02a5c93db0b436396b7e1
pengchao2022 <pengchao.ma2@outlook.com>
2024-09-06 12:48:53 +0800

    A simple while loop

--- while_infinete_loop.sh
+++ while_infinete_loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while :
+while True :
 
 do
 	echo "helloworld!"
---
+++ while_loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# this is a simple example for while loop
+
+n=1
+
+while [ $n -le 10 ]
+
+do
+
+	echo "$n"
+
+        n=$(( $n+1 ))
+
+done
+
+
+
+
+
