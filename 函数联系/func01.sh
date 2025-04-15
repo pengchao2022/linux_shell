7f8a259cee54d91bed0fa897f9c60009260fe1b6
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2025-04-15 08:46:51 +0800

    Create func01.sh

---
+++ 函数联系/func01.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+#函数的定义
+xianshi(){
+
+	echo "Hello Disney land!"
+}
+
+#函数的调用，注意，后面没有()
+xianshi
+
+
+
+allen@ubuntu:~/shell$ source func01.sh 
+Hello Disney land!
+allen@ubuntu:~/shell$ 
+
