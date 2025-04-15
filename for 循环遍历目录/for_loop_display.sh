7aa79080ff5838940883cae3c869b812788fc5aa
Spiderman_git <118711195+pengchao2022@users.noreply.github.com>
2025-04-15 08:37:34 +0800

    Create for_loop_display.sh

---
+++ for 循环遍历目录/for_loop_display.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+for i in $(ls /home/allen/shell)
+
+do
+	echo ${i}
+        sleep 2
+done
+
+
+allen@ubuntu:~/shell$ source bianli.sh 
+-EOF
+bianli.sh
+chengji.txt
+create_user.sh
+docker
+if_then_else.sh
+jingdong.txt
+my2025.txt
+myfile.txt
+myshell01.sh
