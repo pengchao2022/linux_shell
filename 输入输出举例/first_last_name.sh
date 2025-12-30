b8cd8ef9fe657461b2ec1b19e33f545332bcba90
普罗米修斯 <118711195+pengchao2022@users.noreply.github.com>
2025-12-30 18:01:37 +0800

    Create first_last_name.sh

---
+++ 输入输出举例/first_last_name.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+printf "please enter your first name here: "
+
+read -r firstname
+
+printf "please enter your last name here: "
+
+read -r lastname
+
+echo "your full name is: $firstname.$lastname"
+
+
+
